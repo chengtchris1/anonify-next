a473f7b3da888b6fef492d0954a5884da434c072
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-06-30 20:06:23 -0700

    Add song to PL by searching

--- src/app/[playlist]/searchSong.tsx
+++ src/app/[playlist]/searchSong.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect, useCallback} from "react";
 import { debounce, throttle } from "lodash";
 import SubmitButton from "./SubmitButton";
+import Image from "next/image";
 export default function SearchSong({searchSong, addSong}) {
   const [songField, setSongField] = useState<string>('');
   const [results, setResults] = useState<any>(null);
@@ -24,20 +25,31 @@ return (
     <>
       <label htmlFor='url' className='px-1'>Search Song</label>
       <input value={songField} onChange={(e)=>{setSongField(e.target.value)}} className="input input-bordered w-full max-w-xs" name="url" type='text'/>
+      <div className="w-1/2">
       {
-        results ? results.tracks.items.map((track: any) => {
+        (results && songField.length >= 1) ? results.tracks.items.map((track: any) => {
           return (
             <form action={addSong}>
-            <div key={track.id} className='flex justify-between'>
-              <span>{track.name}</span>
-              <input name="url" value={track.id}/>
+            <div key={track.id} className='grid grid-cols-7 border rounded-2xl p-2 m-2'>
+              <div className="col-span-2">
+                <Image width={100} height={100} src={track.album.images[0].url}/>
+              </div>
+              <div className="col-span-2 flex items-center">
+                <span>{track.name}</span>
+                </div>
+              <div className="col-span-2 flex items-center">
+              <input className='hidden' name="url" value={track.id}/>
               <span>{track.artists.map((artist) => artist.name).join(', ')}</span>
+              </div>
+              <div className="col-span-1 flex items-center">
               <SubmitButton/>
+              </div>
             </div>
             </form>
           )
         } ) : null
       }
+      </div>
     </>
 );
 }
